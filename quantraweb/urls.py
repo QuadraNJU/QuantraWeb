e3e430af0842ed425fe2f55752e83d024ea73a6e
151250091 <151250091@smail.nju.edu.cn>
2017-05-09 21:18:11 +0800

    Add: UI container

--- quantraweb/urls.py
+++ quantraweb/urls.py
@@ -21,7 +21,7 @@ from stock import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', TemplateView.as_view(template_name='index.html')),
+    url(r'^$', TemplateView.as_view(template_name='container.html')),
 
     url(r'^api/stock/market', views.market)
 ]
